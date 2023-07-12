07d1dc2d2c6626add15a55283e7fdc6cfe2bb50d
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-07-12 11:02:17 -0400

    New commit

---
+++ src/components/Board.js
@@ -0,0 +1,20 @@
+import Squere from './Squere';
+
+function Board(props) {
+    return (
+        <div className="board">
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+            <Squere />
+
+        </div>
+    );
+}
+
+export default Board;
---
+++ src/components/Squere.js
@@ -0,0 +1,8 @@
+function Squere(props) {
+    return (
+       <div>
+         <h4>squere</h4>
+        </div>
+    );
+}
+export default Squere;
